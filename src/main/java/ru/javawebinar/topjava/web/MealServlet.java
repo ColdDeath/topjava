dd1f90f5d7b3bb64dbcf8fd5ca22b9cf1122d493
ColdDeath <SlavaM4@yandex.ru>
2016-12-12 19:01:02 +0300

    Base Complete

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,53 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Created by Administrator on 12.12.2016.
+ */
+public class MealServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(MealServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to meals");
+
+        String forward = "";
+        String action = request.getParameter("action");
+
+        if (action.equalsIgnoreCase("delete")) {
+            int mealId = Integer.parseInt(request.getParameter("mealId"));
+
+          /*  dao.deleteUser(userId);
+            forward = LIST_USER;
+            request.setAttribute("users", dao.getAllUsers());
+        } else if (action.equalsIgnoreCase("edit")){
+            forward = INSERT_OR_EDIT;
+            int userId = Integer.parseInt(request.getParameter("userId"));
+            User user = dao.getUserById(userId);
+            request.setAttribute("user", user);
+        } else if (action.equalsIgnoreCase("listUser")){
+            forward = LIST_USER;
+            request.setAttribute("users", dao.getAllUsers());
+        } else {
+            forward = INSERT_OR_EDIT;
+        }
+*/
+
+
+            request.setAttribute("mealList", MealsUtil.getWithExceeded(MealsUtil.MEAL_LIST, 2000));
+            //response.sendRedirect("meals.jsp");
+            request.getRequestDispatcher("/meals.jsp").forward(request, response);
+        }
+    }
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,32 @@
+<%@ page import="ru.javawebinar.topjava.util.TimeUtil" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+    <style>
+        .normal {color: green;}
+        .exceeded {color: red;}
+    </style>
+</head>
+<body>
+<table>
+    <thead>
+        <th>DateTime</th>
+        <th>Description</th>
+        <th>Calories</th>
+    </thead>
+    <c:forEach items="${mealList}" var="meal">
+        <jsp:useBean id="meal" scope="page" type="ru.javawebinar.topjava.model.MealWithExceed"/>
+        <tr class="${meal.exceed ? 'exceeded' : 'normal'}">
+            <td>
+                <%=TimeUtil.toString(meal.getDateTime())%>
+            </td>
+            <td>${meal.description}</td>
+            <td>${meal.calories}</td>
+        </tr>
+
+    </c:forEach>
+</table>
+</body>
+</html>
